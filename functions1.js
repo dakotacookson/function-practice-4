9a502fa657b0fd65ff505080f97b94e3913f4533
dakotacooksongit config --global user.namegit config --global user.name dakotacookson <dakota.cookson1995@gmail.com>
2018-11-27 10:54:50 -0600

    final

---
+++ functions1.js
@@ -0,0 +1,11 @@
+const TheKing = {
+    firstName: "Dakota",
+    Lastname: "Cookson",
+    age: 22
+};
+
+function displayName() {
+ console.log(TheKing.firstName , TheKing.Lastname)
+ return displayName;
+}
+displayName();
